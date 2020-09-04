7cbc8a6e42283ddae7ba92574b5740fdb472a9bf
Tyler E <69095276+Tyler-Edwards-E@users.noreply.github.com>
2020-09-04 15:32:46 -0400

    Update Matches.py

--- ChessScraper/ChessScraper/spiders/Matches.py
+++ ChessScraper/ChessScraper/spiders/Matches.py
@@ -30,10 +30,22 @@ class ChessSpider3(scrapy.Spider):
 
             for match in response.xpath('//tr[contains(@class, "v-board-popover")]'):
                 format = match.xpath('.//span[contains(@class, "archive-games-game-time")]/text()').extract_first().strip()
+
+                titles = match.xpath('.//a[contains(@class, "post-view-meta-title v-tooltip")]/text()').getall()
+                titles = [x.strip() for x in titles]
+
                 name = match.xpath('.//a[contains(@class, "post-view-meta-username v-user-popover")]/text()').getall()
                 name = [x.strip() for x in name]
-                country = match.xpath('.//div[contains(@class, "archive-games-result-wrapper-score")]/div/text()').getall()
+
+                ratings = match.xpath('.//span[contains(@class, "post-view-meta-rating")]/text()').getall()
+
+                country = match.xpath('.//div[contains(@class, "post-view-meta-user  ")]/div/@v-tooltip').getall()
                 country = [x.strip() for x in country]
+
+                accuracy = match.xpath('.//td[contains(@class, "table-text-center")]/div/text()').getall()
+
+                moves = match.xpath('.//td[contains(@class, "table-text-center")]/span/text()').extract()
+
                 date_played = match.xpath('.//td[contains(@class, "table-text-right archive-games-date-cell")]').extract_first().split()
                 date_played = date_played[-4] + ' ' + date_played[-3] + date_played[-2]
                 # =========================== PRINT TEST ==========================================
@@ -42,8 +54,12 @@ class ChessSpider3(scrapy.Spider):
                 print("==================================================================================================")
                 # print(match)
                 print(format) # Format
+                print(titles) ############ Need solution to only one player having a title
                 print(name) # Usernames
+                print(ratings)
                 print(country)
+                print(moves)
+                print(accuracy)
                 print(date_played)
                 print("==================================================================================================")
                 print()
