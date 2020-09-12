aa10e419aed7683f42104cce2c6cd50c86455516
Tyler E <69095276+Tyler-Edwards-E@users.noreply.github.com>
2020-09-12 19:10:17 -0400

    Add files via upload

---
+++ (Users)-Data-Analysis.R
@@ -0,0 +1,9 @@
+
+# Tyler Edwards
+# 9 - 12 - 2020
+# Chess.com Users Data Analysis
+
+D = read.csv("ChessScraper/[Chess.com]-Users.csv")
+str(D) # Checking that all data types are correct
+
+# *** The list of usernames in this dataset were not collected randomly, so there may be bias in the analysis.
