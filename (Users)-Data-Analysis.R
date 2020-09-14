7544810b4cecef7ebb11e4ca4922b66f68ab4155
Tyler E <69095276+Tyler-Edwards-E@users.noreply.github.com>
2020-09-13 22:38:39 -0400

    Update (Users)-Data-Analysis.R

--- (Users)-Data-Analysis.R
+++ (Users)-Data-Analysis.R
@@ -7,3 +7,12 @@ D = read.csv("ChessScraper/[Chess.com]-Users.csv")
 str(D) # Checking that all data types are correct
 
 # *** The list of usernames in this dataset were not collected randomly, so there may be bias in the analysis.
+
+hist(D$Blitz)
+# First example of bias
+# The username list started with higher ranked players, so the following users are also likely to be high ranked
+hist(D$Bullet)
+
+CountryCount = as.data.frame(table(D$Country))
+
+
