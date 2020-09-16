ce9fcfc10fa077add623cdbf5fc59a56fdf6a417
Tyler E <69095276+Tyler-Edwards-E@users.noreply.github.com>
2020-09-15 20:51:44 -0400

    Add files via upload

--- (Users)-Data-Analysis.R
+++ (Users)-Data-Analysis.R
@@ -11,7 +11,15 @@ str(D) # Checking that all data types are correct
 hist(D$Blitz)
 # First example of bias
 # The username list started with higher ranked players, so the following users are also likely to be high ranked
-hist(D$Bullet)
+
+par(mfrow=c(3,4))
+hist(D$Blitz); hist(D$Bullet); hist(D$Rapid); hist(D$Puzzle.Rush); hist(D$Puzzles)
+hist(D$Daily.960); #hist(D$Daily); 
+hist(D$Live.960); hist(D$X3.Check); hist(D$King.of.the.Hill)
+hist(D$Crazyhouse); hist(D$Bughouse);
+# Daily is not numeric at the moment
+
+
 
 CountryCount = as.data.frame(table(D$Country))
 
