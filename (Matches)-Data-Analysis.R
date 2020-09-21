daaf3345e5d2cab2d6255568f7868fe17d04dbf5
Tyler E <69095276+Tyler-Edwards-E@users.noreply.github.com>
2020-09-20 23:50:45 -0400

    Add files via upload

---
+++ (Matches)-Data-Analysis.R
@@ -0,0 +1,11 @@
+
+# Tyler Edwards
+# 9 - 12 - 2020
+# Chess.com Match Data Analysis
+
+D = read.csv("ChessScraper/[Chess.com]-Matches.csv")
+str(D) # Checking that all data types are correct
+
+# Difference between rating (White - Black)
+# Analysis showing how much rating influence matches
+
