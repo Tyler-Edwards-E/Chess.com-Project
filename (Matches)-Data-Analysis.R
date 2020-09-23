5973c3c85b1fe1940adcd52976c4b61a85b4d2c4
Tyler E <69095276+Tyler-Edwards-E@users.noreply.github.com>
2020-09-22 23:22:56 -0400

    Add files via upload

--- (Matches)-Data-Analysis.R
+++ (Matches)-Data-Analysis.R
@@ -6,6 +6,12 @@
 D = read.csv("ChessScraper/[Chess.com]-Matches.csv")
 str(D) # Checking that all data types are correct
 
+D$Date_Collected = as.Date(D$Date_Collected, "%m/%d/%Y")
+
+par(mfrow=c(3,2))
+hist(D$Moves_Played); hist(D$White_Rating); hist(D$White_Accuracy)
+hist(D$Black_Rating); hist(D$Black_Accuracy); 
+
 # Difference between rating (White - Black)
 # Analysis showing how much rating influence matches
 # Accuracy Difference
