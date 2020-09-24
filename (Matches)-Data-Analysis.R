43d423a76520a00cc1c7dc166f7bdb2d37614b03
Tyler E <69095276+Tyler-Edwards-E@users.noreply.github.com>
2020-09-23 22:12:10 -0400

    Add files via upload

--- (Matches)-Data-Analysis.R
+++ (Matches)-Data-Analysis.R
@@ -8,9 +8,13 @@ str(D) # Checking that all data types are correct
 
 D$Date_Collected = as.Date(D$Date_Collected, "%m/%d/%Y")
 
+FormatCount = as.data.frame(table(D$Format))
+# 3 min (Blitz) is the highest but there are also some weird formats like "5 days" that I don't know the meaning of
+Countrys = merge(D$White_Country, D$Black_Country)
+
 par(mfrow=c(3,2))
 hist(D$Moves_Played); hist(D$White_Rating); hist(D$White_Accuracy)
-hist(D$Black_Rating); hist(D$Black_Accuracy); 
+hist(D$Black_Rating); hist(D$Black_Accuracy)
 
 # Difference between rating (White - Black)
 # Analysis showing how much rating influence matches
